98bad36c03d1e365b1f048b805a63d3800952642
ehalbertsma <59008238+ehalbertsma@users.noreply.github.com>
2022-02-13 21:32:17 -0500

    impoved functionality

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pomodoro
 DIY pomodoro timer
+
+
+## future features
+
+- multiprocessing to get the timer to run concurrently
+- optimizing the for loop timing
+- visual progress bar
+- basic GUI
+- advanced parameters (increasing time_period with each iteration)
+- countdown instead of up
--- pomodoro.py
+++ pomodoro.py
@@ -7,7 +7,7 @@
 import time
 import os
 
-# set parameters
+# optional: user-set parameters
 
 try: # focus time [ minutes ]
     focus_time = float(input("Focus time (min): "))
@@ -27,7 +27,7 @@ reps_done = 0 # number of pomodoros done [ unitless ]
 N_chime = 3 # number of chimes at the end of an interval
 t_0 = time.time() # set zero time [ seconds, float ]
 focus_state = True # current state of timer [ Boolean ]
-chime_path = '/System/Library/Sounds/Ping.aiff'
+chime_path = 'Ping.aiff'
 
 
 def chime(N):
@@ -39,12 +39,13 @@ def interval(time_period, focus_state, t_0, reps_done, N_chime):
     # time_period:    length of interval in minutes
     # t_0:            starting time
     
-    print("Focus", reps_done) if focus_state else print("Break", reps_done)
     
-    for j in range(int(time_period * 60)):
+    print("Focus", reps_done, end="   \n") if focus_state else print("Break", reps_done, end="   \n")
+    
+    for j in range(int(60*time_period)):
 
-        delta_t = int(time.time() - t_0) + 1
-        print(delta_t, flush=True)
+        delta_t = time.strftime("%H:%M:%S", time.gmtime(time.time() - t_0 + 1))
+        print(delta_t, end="\r")
         time.sleep(1)
         
     chime(N_chime)
@@ -62,7 +63,7 @@ for i in range(reps_goal):
     focus_state, reps_done, t_0 = interval(focus_time, focus_state, t_0, reps_done, N_chime)
 
     #break interval
-    focus_state, reps_done, t_0 = interval(short_break, focus_state, t_0, reps_done, N_chime)
+    focus_state, reps_done, t_0 = interval(short_break, focus_state, t_0, reps_done - 1, N_chime)
 
 print("Done")
     
